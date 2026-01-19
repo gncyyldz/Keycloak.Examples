29403613822453adb09ed25220bb7c04dfe412af
genca <genca@MSI>
2026-01-20 00:04:28 +0300

    Add project files.

---
+++ Shared/Modals/Request/ClientRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Modals.Request
+{
+    public class ClientRequest
+    {
+        [JsonPropertyName("clientId")]
+        public string ClientId { get; set; }
+
+        [JsonPropertyName("enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonPropertyName("protocol")]
+        public string Protocol { get; set; }
+
+        [JsonPropertyName("publicClient")]
+        public bool PublicClient { get; set; }
+
+        [JsonPropertyName("serviceAccountsEnabled")]
+        public bool ServiceAccountsEnabled { get; set; }
+
+        [JsonPropertyName("directAccessGrantsEnabled")]
+        public bool DirectAccessGrantsEnabled { get; set; }
+    }
+}
---
+++ Shared/Modals/Request/UserRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Modals.Request
+{
+    public class UserRequest
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("emailVerified")]
+        public bool EmailVerified { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+    }
+}
